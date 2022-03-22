5e760808fdd5cf818e66ba317f86869349913da3
Xander <roper.alexander@gmail.com>
2022-03-22 13:23:28 -0400

    Initial Commit

---
+++ Junker/JunkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunkerApp.swift
+//  Junker
+//
+//  Created by Xander on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct JunkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
